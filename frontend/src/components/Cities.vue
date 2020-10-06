1c021bc9ccbb042713861fbd2f0c9f0507d0ab73
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-06 22:48:26 +0200

    started frontend

---
+++ frontend/src/components/Cities.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "Cities",
+  mounted() {
+    axios.get("http://localhost:8080/v1/cities?country=PL")
+        .then(function (res) {
+          console.log(res)
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Cities />
+  </div>
+</template>
+
+<script>
+import Cities from "@/components/Cities";
+export default {
+  name: 'Main',
+  components: {Cities},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
