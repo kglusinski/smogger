2961238756eb6e8c69481820fbb2b8e76c93cf6e
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-06 19:12:48 +0200

    added simple ping route; added gorilla/mux

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 )
 
 func main() {
 	log.Println("Starting server - Smogger v1.0 by Kamil Głusiński")
+
+	handle()
+}
+
+func handle() {
+	router := mux.NewRouter().StrictSlash(true)
+	sRouter := router.PathPrefix("/v1").Subrouter()
+
+	sRouter.HandleFunc("/", whoami)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func whoami(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"name": "Smogger v1"}`))
 }
