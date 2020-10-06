66082620ff29c84a0104f527040ef4e622f1e842
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-06 21:39:27 +0200

    refactored client

--- internal/openaq/client.go
+++ internal/openaq/client.go
@@ -13,15 +13,12 @@ const DateISO = "2006-01-02"
 // Client is a http client for https://api.openaq.org
 type Client struct{}
 
+// NewClient creates new openaq API client
 func NewClient() *Client {
 	return &Client{}
 }
 
-type Response struct {
-	Results json.RawMessage `json:"results"`
-}
-
-// Cities sends request to the `/cities` endpoint and return list of cities in the country
+// Cities sends request to the `/cities` endpoint and returns list of cities in the country
 func (c *Client) Cities(country string, v interface{}) error {
 	res, err := http.Get(fmt.Sprintf("https://api.openaq.org/v1/cities?country=%s", country))
 	if err != nil {
@@ -29,25 +26,10 @@ func (c *Client) Cities(country string, v interface{}) error {
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("body data corrupted, err %v", err)
-	}
-
-	var result Response
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return fmt.Errorf("unmarshal failed, err %v", err)
-	}
-
-	err = json.Unmarshal(result.Results, v)
-	if err != nil {
-		return fmt.Errorf("unmarshal failed, err %v", err)
-	}
-
-	return nil
+	return process(res, v)
 }
 
+// Measurements sends request to the `/measurements` endpoint and returns list of measurements in the city
 func (c *Client) Measurements(city string, dateFrom, dateTo time.Time, v interface{}) error {
 	res, err := http.Get(fmt.Sprintf("https://api.openaq.org/v1/measurements?city=%s&date_from=%s&date_to=%s", city, dateFrom.Format(DateISO), dateTo.Format(DateISO)))
 	if err != nil {
@@ -55,12 +37,20 @@ func (c *Client) Measurements(city string, dateFrom, dateTo time.Time, v interfa
 	}
 	defer res.Body.Close()
 
+	return process(res, v)
+}
+
+type response struct {
+	Results json.RawMessage `json:"results"`
+}
+
+func process(res *http.Response, v interface{}) error {
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return fmt.Errorf("body data corrupted, err %v", err)
 	}
 
-	var result Response
+	var result response
 	err = json.Unmarshal(body, &result)
 	if err != nil {
 		return fmt.Errorf("unmarshal failed, err %v", err)
