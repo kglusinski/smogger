3f459fba60088b36721be5510afd14d81a5edb9d
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-07 21:05:56 +0200

    url encoded

--- internal/openaq/client.go
+++ internal/openaq/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -31,7 +32,7 @@ func (c *Client) Cities(country string, v interface{}) error {
 
 // Measurements sends request to the `/measurements` endpoint and returns list of measurements in the city
 func (c *Client) Measurements(city string, dateFrom, dateTo time.Time, v interface{}) error {
-	res, err := http.Get(fmt.Sprintf("https://api.openaq.org/v1/measurements?city=%s&date_from=%s&date_to=%s&parameter=pm25", city, dateFrom.Format(DateISO), dateTo.Format(DateISO)))
+	res, err := http.Get(fmt.Sprintf("https://api.openaq.org/v1/measurements?city=%s&date_from=%s&date_to=%s&parameter=pm25", url.QueryEscape(city), dateFrom.Format(DateISO), dateTo.Format(DateISO)))
 	if err != nil {
 		return fmt.Errorf("http request failed, err %v", err)
 	}
